b1640530c9fdafc46cb7b6efd4d34dc6d6bcb707
RudeSoul <prabesh7@gmail.com>
2020-07-01 02:22:12 +0545

    completed with background image part

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import NavBar from "./components/layout/NavBar";
 // import Dashbord from "./components/layout/Dashboard";
 import Dashboard from "./components/layout/Dashboard";
 
+import backgroundImage from "./components/assets/bg.jpg";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ background: `url(${backgroundImage})` }}>
       <NavBar></NavBar>
       <div className="container">
         <Dashboard></Dashboard>
