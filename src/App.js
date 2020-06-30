055ea1408908507007529b9dbbcce5ab943a5865
RudeSoul <prabesh7@gmail.com>
2020-06-30 23:28:11 +0545

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
 }
 
 export default App;
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 export default class NavBar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <span>POKEDEX</span>
+        </div>
+      </div>
+    );
   }
 }
